688495152490f5f774d47670580ed3628d7b1994
hp <hp@DESKTOP-FD3N71H>
2023-02-27 15:48:22 +0530

    .

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,20 +1,74 @@
 ﻿
 class program { 
   public static void Main()
-{
-    string FirstName, MiddleName, LastName;
+    {
+        string FirstName, MiddleName, LastName;
 
-    Console.WriteLine("Please Enter First Name");
-    FirstName = Console.ReadLine();
+        Console.WriteLine("Please Enter First Name");
+        FirstName = Console.ReadLine();
 
-    Console.WriteLine("Please Enter Middle Name");
-    MiddleName = Console.ReadLine();
+        Console.WriteLine("Please Enter Middle Name");
+        MiddleName = Console.ReadLine();
 
-    Console.WriteLine("Please Enter Last Name");
-    LastName = Console.ReadLine();
+        Console.WriteLine("Please Enter Last Name");
+        LastName = Console.ReadLine();
 
-    string FullName = FirstName +" "+ MiddleName +" "+ LastName ;
+        string FullName = FirstName + " " + MiddleName + " " + LastName;
 
-    Console.WriteLine(FullName);
+        Console.WriteLine(FullName);
+        Console.WriteLine("----------------------------------------------------------------");
+
+        Program1 program1= new Program1();
+        program1.Pattern();
+        Program2 program2= new Program2();
+        program2.Pattern(); 
+      
+    }
 }
+ class Program1
+{
+    public void Pattern()
+    {
+        string FirstName, MiddleName, LastName;
+
+        Console.WriteLine("Please Enter First Name");
+        FirstName = Console.ReadLine();
+
+        Console.WriteLine("Please Enter Middle Name");
+        MiddleName = Console.ReadLine();
+
+        Console.WriteLine("Please Enter Last Name");
+        LastName = Console.ReadLine();
+
+        string FullName = FirstName + " " + MiddleName + " " + LastName;
+
+        Console.WriteLine(FullName);
+        Console.WriteLine("----------------------------------------------------------------");
+    }
+    
+    
+}
+
+class Program2
+{
+    public void Pattern()
+    {
+        string FirstName, MiddleName, LastName;
+
+        Console.WriteLine("Please Enter First Name");
+        FirstName = Console.ReadLine();
+
+        Console.WriteLine("Please Enter Middle Name");
+        MiddleName = Console.ReadLine();
+
+        Console.WriteLine("Please Enter Last Name");
+        LastName = Console.ReadLine();
+
+        string FullName = FirstName + " " + MiddleName + " " + LastName;
+
+        Console.WriteLine(FullName);
+        Console.WriteLine("----------------------------------------------------------------");
+    }
+
+
 }
